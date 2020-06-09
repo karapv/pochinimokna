0978b8e3d0f2e5b4c4b6ded073ade9d53bee0306
Andrew <andrew.optmzg@gmail.com>
2020-06-09 16:45:02 +0200

    add new fixes

--- app/js/_custom.js
+++ app/js/_custom.js
@@ -184,12 +184,12 @@ document.addEventListener("DOMContentLoaded", function() {
     const scrollTo = (elem,attr) => {
         $(elem).on("click", function () {
             let anchor = $(this).attr(attr);
-            console.log(anchor);
             $('html, body').stop().animate({
-                scrollTop: $('#'+anchor).offset().top - 60
+                scrollTop: $(anchor).offset().top - 60
             }, 1500);
         });
     };
+    scrollTo('.scrollTo a','href')
     //Tab func
     const tabElem = (item,container,name) =>{
         $(`${item}:first-child`).addClass('active');
